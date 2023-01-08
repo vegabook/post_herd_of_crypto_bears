e98a7118d3fffc650f817e1d6ba1e301fbd32c7c
vegabook <thomas@crvm.io>
2023-01-08 23:55:21 +0000

    buy chart added

--- main.r
+++ main.r
@@ -23,7 +23,10 @@ lbi <- logret(bi)
 x11(width = 10, height = 6)
 par(mfrow = c(1, 2))
 regress(lei, lti[, 2], legnd = F, hiDays = c(0, 100, 100), xlab = "ETH 1 month return", ylab = "Google trends 'crypto scam' same month return", main = "Price vs 'scam' search trend")
-regress(lbi[1:(nrow(lbi) - 1), ], lti[2:nrow(lti), 2], hiDays = c(100, 100, 100), legnd = F, diagnostics = T, xlab = "ETH 1 month return", ylab = "Google trends 'crypto scam' following month return", main = "...following month hindsight")
+regress(lei[1:(nrow(lei) - 1), ], lti[2:nrow(lti), 2], hiDays = c(100, 100, 100), legnd = F, diagnostics = T, xlab = "ETH 1 month return", ylab = "Google trends 'crypto scam' following month return", main = "...following month hindsight")
+
+dev.new()
+regress(lti[1:(nrow(lti) - 1), 2], lei[2:nrow(lei), ], hiDays = c(0, 0, 100), legnd = F, diagnostics = T, ylab = "ETH next month return", xlab = "Google trends 'crypto scam' monthly return", main = "Buy when the bears are hurting")
 
 
 
